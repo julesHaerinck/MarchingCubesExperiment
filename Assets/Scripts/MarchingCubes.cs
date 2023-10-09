4d1879ce645f192ddcd36f9e23ea63f261925952
julesHaerinck <haerinck.jules@gmail.com>
2023-10-09 15:27:30 +0200

    First Commit

---
+++ Assets/Scripts/MarchingCubes.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarchingCubes : MonoBehaviour
+{
+    //public Vector3 BoundingBox = Vector3.one;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
