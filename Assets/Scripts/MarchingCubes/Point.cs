c9e03d42422a708923cd443bc5115382aa79dbbb
julesHaerinck <haerinck.jules@gmail.com>
2023-10-10 20:17:29 +0200

    Added MarchingCube namespace

--- Assets/Scripts/MarchingCubes/Point.cs
+++ Assets/Scripts/MarchingCubes/Point.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Point
+
+namespace MarchingCube
 {
-    private float   _isosurfaceValue;
-    private Vector3 _position;
-
-    public float   IsosurfaceValue { get; set; }
-    public Vector3 Position { get; set; }
-
-    public Point(float isosurfaceValue, Vector3 position)
+    public class Point
     {
-        _isosurfaceValue = isosurfaceValue;
-        _position = position;
-    }
+        private float   _isosurfaceValue;
+        private Vector3 _position;
 
+        public float IsosurfaceValue { get; set; }
+        public Vector3 Position { get; set; }
+
+        public Point(float isosurfaceValue, Vector3 position)
+        {
+            _isosurfaceValue = isosurfaceValue;
+            _position = position;
+        }
+
+    }
 }
