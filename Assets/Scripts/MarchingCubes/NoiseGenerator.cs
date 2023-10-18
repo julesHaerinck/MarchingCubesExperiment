a40400c591b17c2b42cc065eb7e56dda94666464
julesHaerinck <haerinck.jules@gmail.com>
2023-10-18 18:51:38 +0200

    Implemented the noise generation for more realistic terrains

--- Assets/Scripts/MarchingCubes/NoiseGenerator.cs
+++ Assets/Scripts/MarchingCubes/NoiseGenerator.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class NoiseGenerator : MonoBehaviour
 {
+	[SerializeField] float _amplitude  = 5f;
+	[SerializeField] float _frequency  = 0.005f;
+	[SerializeField] int   _octaves    = 8;
+	[SerializeField, Range(0f,1f)] 
+	float _groundPercentage = 0.2f;
+
 	public ComputeShader NoiseCompute;
 
 	ComputeBuffer _weightsBuffer;
@@ -33,8 +39,16 @@ public class NoiseGenerator : MonoBehaviour
 	{
 		float[] noiseValues = new float[GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk * GridMetrics.PointsPerChunk];
 
-		NoiseCompute.SetBuffer(0, "_Weights", _weightsBuffer);
-		NoiseCompute.Dispatch(
+		NoiseCompute.SetBuffer(0, "_Weights"  , _weightsBuffer);
+		NoiseCompute.SetInt  ("_ChunkSize"    , GridMetrics.PointsPerChunk);
+		NoiseCompute.SetFloat("_Amplitude"    , _amplitude);
+		NoiseCompute.SetFloat("_Frequency"    , _frequency);
+		NoiseCompute.SetFloat("_GroundPercent", _groundPercentage);
+		NoiseCompute.SetInt  ("_Octaves"      , _octaves);
+
+
+
+        NoiseCompute.Dispatch(
 				0, 
 				GridMetrics.PointsPerChunk / GridMetrics.NumThreads, 
 				GridMetrics.PointsPerChunk / GridMetrics.NumThreads, 
